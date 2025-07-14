ef031f3582b82472346fbf97042472f00ecd8371
RamiyanGangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2025-07-14 09:43:16 -0400

    server mongo fix

--- server/config/db.js
+++ server/config/db.js
@@ -2,21 +2,32 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const MONGO_URI = process.env.MONGODB_URI;
-const client = new MongoClient(MONGO_URI);
+
+const client = new MongoClient(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  tlsAllowInvalidCertificates: false,
+});
 
 let db;
 
 async function connectToDB() {
   if (!db) {
-    await client.connect();
-    db = client.db('website'); 
-    console.log('Connected to MongoDB');
+    try {
+      await client.connect();
+      db = client.db('website'); 
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('MongoDB connection error:', error);
+      throw error;
+    }
   }
   return db;
 }
 
 async function closeDB() {
-  if (client.isConnected()) {
+  if (client && client.topology && client.topology.isConnected()) {
     await client.close();
     console.log('MongoDB connection closed');
   }
