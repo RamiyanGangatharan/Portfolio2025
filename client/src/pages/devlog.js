80b2f736d3531b924cad906fc21f5efdec85b2c8
RamiyanGangatharan <ramiyan2185@gmail.com>
2026-01-05 22:40:55 -0500

    fixes after more testing

--- client/src/pages/devlog.js
+++ client/src/pages/devlog.js
@@ -7,7 +7,7 @@ function DevLogEntry({ date, sections }) {
   return (
     <div className="mb-4">
       {/* Date badge */}
-      <div className="mb-2">
+      <div className="mb-2 text-center text-md-start">
         <span className="badge bg-light text-dark px-3 py-2 fs-6">
           {date}
         </span>
@@ -40,8 +40,8 @@ export default function DevLog() {
 
       <Header />
 
-      <main className="container my-3">
-        <div className="devlog card bg-dark text-white shadow-lg border-0 p-3 p-sm-4">
+      <main className="container-fluid px-2 px-md-4 my-3">
+        <div className="devlog card bg-dark text-white shadow-lg border-0 p-3 p-md-4">
           <h1 className="display-5 text-center mb-4">Development Log</h1>
 
           {devLogs.map((log, i) => (
