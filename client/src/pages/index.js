2925347201b79452f77b62f69d0179b798776dea
Ramiyan Gangatharan <ramiyan2185@gmail.com>
2026-04-01 14:11:16 -0400

    jobs

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -45,9 +45,49 @@ export default function Home() {
         </div>
 
         <div className="text-white bg-dark shadow-lg border-0 p-5 mb-5 mt-5 rounded w-100">
+          <h2 className="display-4 fw-bold">Experience &amp; Projects</h2>
+          <hr className="w-50 mx-auto opacity-25" />
 
+          <div className="card bg-light border-0 shadow-lg mb-4 mt-4">
+            <div className="card-body">
+              <p className="card-title text-dark display-5">The Mixed Reality Capture Studio</p>
+              <p className="card-text text-dark fw-bold fst-italic mb-2">Research Assistant: Designer &amp; Developer Intern</p>
+              <p className="card-text text-dark mb-2"> [ April - May 2025 | Durham College ]</p>
+              <hr className="w-75 mx-auto opacity-25" />
+              <div className="ms-2">
+                <ul className="text-start ms-5 text-dark">
+                  <li>Developed a Unity-based AI assistant for VR/XR &amp; desktop using local LLMs</li>
+                    <ul>
+                      <li>Integrated voice dictation + text-to-speech with a REST pipeline using FastAPI</li>
+                      <li>Built real-time speech-to-AI interaction with transcription, JSON requests, &amp; live responses</li>
+                      <li>Prototyping conversational systems for immersive environments</li>
+                    </ul>
+                  
+                  <li>Building a Souls-like 3D game</li>
+                  <ul>
+                    <li>Implemented player movement, camera systems, animations, abilities, &amp; collisions</li>
+                    <li>Focused on responsive combat &amp; immersive level design</li>
+                    <li>Strengthened skills in Unity, AI integration, &amp; cross-platform development</li>
+                  </ul>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-light border-0 shadow-lg mb-4 mt-4">
+            <div className="card-body">
+              <p className="card-title text-dark display-5">Tim Hortons</p>            
+              <p className="card-text text-dark mb-2"> [ November 2021 - Present | Various Locations ]</p>
+              <hr className="w-75 mx-auto opacity-25" />
+              <div className="ms-2">
+                <ul className="text-start ms-5 text-dark">
+                
+                    <li>Shift Supervisor</li>
+                  </ul>
+              </div>
+            </div>
+          </div>
         </div>
-
       </main>
       <Footer />
     </>
